1aba4fc1e1e68ec2bd24665eac9c51d8e33bdc74
Bostjan Jerko <bostjan@japina.eu>
2014-04-21 07:50:06 +0200

    menu added

--- chess_clock.ino
+++ chess_clock.ino
@@ -4,10 +4,10 @@
 #define LCD_BACKLIGHT_PIN         3  // D3 controls LCD backlight
 // ADC readings expected for the 5 buttons on the ADC input
 #define RIGHT_10BIT_ADC           0  // right
-#define UP_10BIT_ADC            145  // up
-#define DOWN_10BIT_ADC          329  // down
+#define UP_10BIT_ADC            97  // up
+#define DOWN_10BIT_ADC          251  // down
 #define LEFT_10BIT_ADC          402  // left
-#define SELECT_10BIT_ADC        741  // right
+#define SELECT_10BIT_ADC        636  // right
 #define BUTTONHYSTERESIS         10  // hysteresis for valid button sensing window
 //return values for ReadButtons()
 #define BUTTON_NONE               0  // 
@@ -20,25 +20,75 @@
 #define LCD_BACKLIGHT_OFF()     digitalWrite( LCD_BACKLIGHT_PIN, LOW )
 #define LCD_BACKLIGHT_ON()      digitalWrite( LCD_BACKLIGHT_PIN, HIGH )
 #define LCD_BACKLIGHT(state)    { if( state ){digitalWrite( LCD_BACKLIGHT_PIN, HIGH );}else{digitalWrite( LCD_BACKLIGHT_PIN, LOW );} }
+#define FISCHER 0
+#define BRONSTEIN_DELAY 1
+#define SIMPLE_DELAY 2
+#define OVERTIME_PENALTY 3
+#define HOUR_GLASS 4
+#define ARRAY_SIZE 5
 
 int timer1_counter;
 int leftClockInSec;
 int rightClockInSec;
 boolean leftButtonPressed;
 boolean rightButtonPressed;
+boolean downButtonPressed;
+boolean upButtonPressed;
+boolean selectButtonPressed;
+boolean showMenuFlag;
+boolean refresh;
+boolean doIncrement;
+int pos=0;
+
 
 LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+
+String showSelection(){
+  String retText;
+  switch(pos){
+    case FISCHER:
+      retText="Fischer";
+      break;
+    case BRONSTEIN_DELAY:
+      retText="Bronstein delay";
+      break;
+    case SIMPLE_DELAY:
+      retText="Simple delay";
+      break;
+    case OVERTIME_PENALTY:
+      retText="Overtime penalty";
+      break;
+    case HOUR_GLASS:
+      retText="Hour glass";
+      break;
+  }
+  return retText;
+}
+
+int showMenu(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Select clock:");
+  lcd.setCursor(0,1);
+  lcd.print(showSelection());   
+}
 
 void setup()
 {
    //button adc input
    pinMode( BUTTON_ADC_PIN, INPUT );         //ensure A0 is an input
    digitalWrite( BUTTON_ADC_PIN, LOW );      //ensure pullup is off on A0
-   // setup timer
-   leftClockInSec = 300; // 5 minutes Fischer
-   rightClockInSec = 300; // 5 minutes Fischer
-   leftButtonPressed = false;
-   rightButtonPressed = false;
+   lcd.begin(16,2); // 16 chars in 2 lines
+  showMenuFlag=true;
+  refresh=true;
+  leftButtonPressed = false;
+  rightButtonPressed = false;
+  doIncrement = true;
+  startClock();
+   
+}
+
+void startClock(){
   // initialize timer1 
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
@@ -53,8 +103,7 @@ void setup()
   TCNT1 = timer1_counter;   // preload timer
   TCCR1B |= (1 << CS12);    // 256 prescaler 
   TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
-  interrupts();             // enable all interrupts    
-   
+  interrupts();             // enable all interrupts
 }
 
 String showTime(int timeInSeconds) {
@@ -79,24 +128,67 @@ ISR(TIMER1_OVF_vect)        // interrupt service routine
   }
   if((leftButtonPressed)&&(rightClockInSec>0)) {
      rightClockInSec--;
-  }     
-  
-  lcd.clear();
-  lcd.print (showTime(leftClockInSec) + "    "+ showTime(rightClockInSec));
+  }   
+  if(!showMenuFlag) {
+     lcd.clear();
+     lcd.print (showTime(leftClockInSec) + "    "+ showTime(rightClockInSec));
+  }
 }
 
 void loop()
 {
    byte button;
+   
+   if(showMenuFlag){
+    if(refresh){
+      showMenu();  
+      refresh=false;
+    }
+   }
+   
    button = ReadButtons();
-  if(button == BUTTON_LEFT) {
-      leftButtonPressed = true;
-      rightButtonPressed = false;      
+   // check if button is pressed and released 
+  if((leftButtonPressed) && (button == BUTTON_NONE)) {
+    if(doIncrement){
+      rightClockInSec += 5; // for Fischer
+      doIncrement=false;
+    }
   }
-  if (button == BUTTON_RIGHT) {    
-      rightButtonPressed = true;
-      leftButtonPressed = false;
+  
+  if((rightButtonPressed) && (button == BUTTON_NONE)) {
+    if(doIncrement){
+      leftClockInSec += 5; // for Fischer
+      doIncrement=false;
+    }
+
   }
+  
+  if((downButtonPressed) && (button == BUTTON_NONE)) { //pressed down and released
+    refresh=true;
+    pos--;    
+    downButtonPressed=false;
+  }
+  if((upButtonPressed) && (button == BUTTON_NONE)) { // pressed up and released
+    refresh=true;
+    pos++;
+    upButtonPressed=false;    
+  }
+  if((selectButtonPressed) && (button == BUTTON_NONE)) {
+    // set up for the chess clock depending on the pos !!!!
+    showMenuFlag=false;
+   // setup timer
+   leftClockInSec = 300; // 5 minutes Fischer
+   rightClockInSec = 300; // 5 minutes Fischer  
+   selectButtonPressed=false;  
+    
+  }
+  
+ if(pos<0){
+    pos=ARRAY_SIZE-1;
+  }
+  if(pos>=ARRAY_SIZE){
+    pos=0;
+   }
 }
 
 /*--------------------------------------------------------------------------------------
@@ -111,20 +203,46 @@ byte ReadButtons()
    //read the button ADC pin voltage
    buttonVoltage = analogRead( BUTTON_ADC_PIN );
    //sense if the voltage falls within valid voltage windows
-   if( buttonVoltage < ( RIGHT_10BIT_ADC + BUTTONHYSTERESIS ) )
+   // right
+   if( buttonVoltage < ( RIGHT_10BIT_ADC + BUTTONHYSTERESIS )
+      && buttonVoltage <= ( RIGHT_10BIT_ADC + BUTTONHYSTERESIS ) )
    {
-     if(leftButtonPressed){
-       leftClockInSec += 5; // add 5 seconds - Fischer       
-     } 
+     leftButtonPressed=false;
+     rightButtonPressed=true;
+     doIncrement=true;
       button = BUTTON_RIGHT;
    }
-   else if(   buttonVoltage >= ( LEFT_10BIT_ADC - BUTTONHYSTERESIS )
+   // left
+   if(   buttonVoltage >= ( LEFT_10BIT_ADC - BUTTONHYSTERESIS )
            && buttonVoltage <= ( LEFT_10BIT_ADC + BUTTONHYSTERESIS ) )
    {
-     if(rightButtonPressed){
-       rightClockInSec += 5; // add 5 seconds - Fischer     
-     }
+     leftButtonPressed=true;
+     rightButtonPressed=false;
+     doIncrement=true;
       button = BUTTON_LEFT;
    }
+   // up
+   if(   buttonVoltage >= ( UP_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( UP_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_UP;
+      upButtonPressed=true;
+      downButtonPressed=false;      
+   }
+   // down
+   if(   buttonVoltage >= ( DOWN_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( DOWN_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_DOWN;
+      upButtonPressed=false;
+      downButtonPressed=true;      
+   } 
+   // select
+   if(   buttonVoltage >= ( SELECT_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( SELECT_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_SELECT;
+      selectButtonPressed=true;
+   } 
    return( button );
 }
