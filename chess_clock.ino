0fc5d8b24a9b50f41f8db78dbac84d1add3d2797
Bostjan Jerko <bostjan@japina.eu>
2014-05-01 21:43:41 +0200

    Corrected time interval to 1 sec

--- chess_clock.ino
+++ chess_clock.ino
@@ -106,10 +106,7 @@ void startClock(){
   TCCR1B = 0;
 
   // Set timer1_counter to the correct value for our interrupt interval
-  //timer1_counter = 64886;   // preload timer 65536-16MHz/256/100Hz
-  //timer1_counter = 64286;   // preload timer 65536-16MHz/256/50Hz
-  //timer1_counter = 34286;   // preload timer 65536-16MHz/256/2Hz
-  timer1_counter = 17143;
+  timer1_counter = 3036;   // preload timer 65536-16MHz/256/1Hz  
   
   TCNT1 = timer1_counter;   // preload timer
   TCCR1B |= (1 << CS12);    // 256 prescaler 
